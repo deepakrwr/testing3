6e611ab58d755b17a34a2c357cc22f256c981e0c
deepakrwr <deepakrwr@gmail.com>
2018-06-20 14:16:05 +0530

    f

---
+++ testing3/eco_slide/slide.php
@@ -0,0 +1,75 @@
+
+  
+  
+
+      <link rel="stylesheet" href="eco_slide/css/style.css">
+
+  
+
+  <div class="container">
+	<div class="row">
+		<div class="col-xs-11 ">
+
+			<div id="carousel" class="carousel slide" data-ride="carousel" data-type="multi" data-interval="2500">
+				<div class="carousel-inner">
+					<div class="item active">
+						<div class="carousel-col">
+							<div class="block" ><img src="eco_slide/img/101.jpg"  alt="" class="img-responsive"/></div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="carousel-col">
+							<div class="block"><img src="eco_slide/img/102.jpg"  alt="" class="img-responsive"/></div>
+						</div>
+					</div>
+                    <div class="item">
+						<div class="carousel-col">
+							<div  class="block"><img src="eco_slide/img/103.jpg"  alt="" class="img-responsive"/></div>
+						</div>
+					</div>
+                    <div class="item">
+						<div class="carousel-col">
+							<div class="block" ><img src="eco_slide/img/104.jpg"  alt="" class="img-responsive"/></div>
+						</div>
+					</div>
+                    <div class="item">
+						<div class="carousel-col">
+							<div class="block"><img src="eco_slide/img/105.jpg"  alt="" class="img-responsive"/></div>
+						</div>
+                        
+					</div>
+                    <div class="item">
+						<div class="carousel-col">
+							<div class="block"><img src="eco_slide/img/106.jpg"  alt="" class="img-responsive"/></div>
+						</div>
+                        
+					</div>
+                    
+					
+				</div>
+
+				<!-- Controls -->
+				<div class="left carousel-control">
+				<a href="#carousel" role="button" data-slide="prev" >
+						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+				</div>
+				<div class="right carousel-control">
+					<a href="#carousel" role="button" data-slide="next" >
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js'></script>
+<script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'></script>
+
+    <script src="eco_slide/js/index.js"></script>
+
+</body>
+</html>
